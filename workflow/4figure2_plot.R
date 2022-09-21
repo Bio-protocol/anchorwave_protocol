06aed4f1a99f76de38a96ad5887fa6e21a661ddf
shuaiwang2 <84169743+shuaiwang2@users.noreply.github.com>
2022-09-21 20:46:59 +0800

    Add files via upload

---
+++ workflow/4figure2_plot.R
@@ -0,0 +1,22 @@
+#Using R to plot a dotplot.
+#R
+library(ggplot2)
+#Read gene position and belong to which chromosome and so on
+data =read.table("align1.anchors", header=TRUE)
+#Select all euchromosomes as factor
+data = data[which(data$refChr %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),]
+data = data[which(data$queryChr %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),]
+data$refChr = factor(data$refChr, levels=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+data$queryCh = factor(data$queryChr, levels=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+#Using ggplot2 to plot a dotplot and beautify it.
+ggplot(data=data, aes(x=queryStart, y=referenceStart))+
+geom_point(size=0.5, aes(color=strand)) +
+facet_grid(refChr~queryChr, scales="free", space="free") +
+labs(x="sorghum", y="maize")+scale_x_continuous(labels=changetoM) +
+scale_y_continuous(labels=changetoM) +
+  theme(axis.line = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(fill =NA,color="black", size=0.5, linetype="solid"),
+        axis.text.y = element_text( colour = "black"),
+        legend.position='none',
+        axis.text.x = element_text(angle=300,hjust=0, vjust=0.5, colour = "black") )
