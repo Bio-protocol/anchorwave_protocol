fa9c969dbbf7850c9fda06111d6c1ecd1871148c
shuaiwang2 <84169743+shuaiwang2@users.noreply.github.com>
2022-11-29 13:27:55 +0800

    Update 3figure4_plot.R.R

--- workflow2-case-study2/3figure4_plot.R.R
+++ workflow2-case-study2/3figure4_plot.R.R
@@ -1,13 +1,14 @@
 setwd("E:\\plot\\maize")
 #Use this piece of R to draw a dotplot.
 library(ggplot2)
+library(svglite)
 #Transform Coordinates using a function.
 changetoM <- function ( position ){
   position=position/1000000;
   paste(position, "M", sep="")
 }
 #Read gene position, belong to which chromosome and so on
-data =read.table("align.anchors", header=TRUE)
+data =read.table("align1.anchors", header=TRUE)
 #Select all euchromosomes as factor, modify the chromosomes number manually.
 data = data[which(data$refChr %in% c("1", "2", "3", "4",
                                      "5", "6", "7", "8", "9", "10")),]
@@ -18,8 +19,8 @@ data$refChr = factor(data$refChr, levels=c("1", "2", "3", "4",
 data$queryCh = factor(data$queryChr, levels=c("1", "2", "3", "4",
                                               "5", "6", "7", "8", "9", "10"))
 #Using ggplot2 to plot a dotplot and beautify it.
-png("E:\\plot\\maize\\figure4.png")
-ggplot(data=data, aes(x=queryStart, y=referenceStart))+
+
+figure4 <- ggplot(data=data, aes(x=queryStart, y=referenceStart))+
   geom_point(size=0.5, aes(color=strand)) +
   facet_grid(refChr~queryChr, scales="free", space="free") +
   labs(x="Mo17", y="B73")+scale_x_continuous(labels=changetoM) +
@@ -30,4 +31,12 @@ ggplot(data=data, aes(x=queryStart, y=referenceStart))+
         axis.text.y = element_text( size=5,colour = "black"),
         legend.position='none',
         axis.text.x = element_text(angle=300,size=6,hjust=0, vjust=0.5, colour = "black") )
+png("figure4.png")
+figure4
+dev.off()
+pdf("figure4.pdf")
+figure4
+dev.off()
+svglite("figure4.svg")
+figure4
 dev.off()
