d5211a5d242f5b1c0b76d47cfbe80e43920cbaf0
shuaiwang2 <84169743+shuaiwang2@users.noreply.github.com>
2023-08-02 23:34:30 +0800

    Add files via upload

---
+++ workflow1-case-study1/3_identifycollinearregion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Applying anchorwave to identify the collinearity and WGD 
+
+anchorwave proali -r Zea_mays.AGPv4.dna.toplevel.fa -i Zea_mays.AGPv4.34.gff3 \
+-a cds.sam -as cds.fa -ar ref.sam -s \
+Sorghum_bicolor.Sorghum_bicolor_NCBIv3.dna.toplevel.fa -n align1.anchors -R 1 \
+-Q 2 
---
+++ workflow1-case-study1/4_figure2_plot.R
@@ -0,0 +1,33 @@
+changetoM <- function ( position ){
+  position=position/1000000;
+  paste(position, "M", sep="")}
+library(ggplot2)
+library(svglite)
+data =read.table("align1.anchors", header=TRUE)
+#Select all euchromosomes as factor
+#modify the chromosomes number manually if we change the species.
+data = data[which(data$refChr %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),]
+data = data[which(data$queryChr %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),]
+data$refChr = factor(data$refChr, levels=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+data$queryCh = factor(data$queryChr, levels=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+
+figure2 <- ggplot(data=data, aes(x=queryStart, y=referenceStart))+
+  geom_point(size=0.5, aes(color=strand)) +
+  facet_grid(refChr~queryChr, scales="free", space="free") +
+  labs(x="sorghum", y="maize")+scale_x_continuous(labels=changetoM) +
+  scale_y_continuous(labels=changetoM) +
+  theme(axis.line = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(fill =NA,color="black", size=0.5, linetype="solid"),
+        axis.text.y = element_text( colour = "black"),
+        legend.position='none',
+        axis.text.x = element_text(angle=300,hjust=0, vjust=0.5, colour = "black") )
+png("figure2.png")  
+figure2
+dev.off()
+pdf("figure2.pdf")
+figure2
+dev.off()
+svglite("figure2.svg")
+figure2
+dev.off()
