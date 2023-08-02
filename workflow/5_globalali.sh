33c84ad72140df8e7bbd67c779b07961e5c3f3b8
shuaiwang2 <84169743+shuaiwang2@users.noreply.github.com>
2023-08-02 23:32:54 +0800

    Delete workflow directory

--- workflow/5_globalali.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Base-pair resolved genome alignment
-
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 -r Zea_mays.AGPv4.dna.toplevel.fa -a cds.sam -as cds.fa -ar ref.sam -s \
-Sorghum_bicolor.Sorghum_bicolor_NCBIv3.dna.toplevel.fa  -n align.anchors -o align.maf -t 1 -R 1 -Q 2 -f align1.f.maf > log 2>&1
-
