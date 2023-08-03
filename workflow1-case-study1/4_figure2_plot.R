71991d8eefb0a2707b15923fb331fc1f5b493b7b
shuaiwang2 <84169743+shuaiwang2@users.noreply.github.com>
2023-08-03 21:25:05 +0800

    Update 4_figure2_plot.R

--- workflow1-case-study1/4_figure2_plot.R
+++ workflow1-case-study1/4_figure2_plot.R
@@ -1,21 +1,18 @@
-changetoM <- function ( position ){
-  position=position/1000000;
-  paste(position, "M", sep="")}
 library(ggplot2)
 library(svglite)
+changetoM <- function ( position ){
+position=position/1000000;
+paste(position, "M", sep="")}
 data =read.table("align1.anchors", header=TRUE)
-#Select all euchromosomes as factor
-#modify the chromosomes number manually if we change the species.
 data = data[which(data$refChr %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),]
 data = data[which(data$queryChr %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),]
 data$refChr = factor(data$refChr, levels=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
 data$queryCh = factor(data$queryChr, levels=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
-
 figure2 <- ggplot(data=data, aes(x=queryStart, y=referenceStart))+
-  geom_point(size=0.5, aes(color=strand)) +
-  facet_grid(refChr~queryChr, scales="free", space="free") +
-  labs(x="sorghum", y="maize")+scale_x_continuous(labels=changetoM) +
-  scale_y_continuous(labels=changetoM) +
+geom_point(size=0.5, aes(color=strand)) +
+facet_grid(refChr~queryChr, scales="free", space="free") +
+labs(x="sorghum", y="maize")+scale_x_continuous(labels=changetoM) +
+scale_y_continuous(labels=changetoM) +
   theme(axis.line = element_blank(),
         panel.background = element_blank(),
         panel.border = element_rect(fill =NA,color="black", size=0.5, linetype="solid"),
